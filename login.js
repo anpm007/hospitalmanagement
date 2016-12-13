aff28e5963265d3c823459853857e7d2734cb099
anpm007 <anupamsinha007@gmail.com>
2016-12-13 08:38:57 +0530

    131220160838

--- login.js
+++ login.js
@@ -4,6 +4,20 @@
 
 
 $(document).ready(function(){
+	
+	$('#register_patient').click(function(){
+		//document.location.href = '../index.html';
+		$('#login_page').css('display','block');
+		$('.user-profile').css('display','block');
+		$('.container').css('opacity','.5');
+	});
+	
+	$('#close_login').click(function(){
+		$('#login_page').css('display','none');
+		$('.user-profile').css('display','none');
+		$('.container').css('opacity','1');
+	});
+	
     var authenticationDetails;
     $("#submit").click(function(){
     	 $.ajax({url:"json/authenticate.json",success:function(result){
